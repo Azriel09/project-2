c5bfcda0a0ca306afe6a31e784020ec4522e4b63
Jhan Marmel <105028202+Azriel09@users.noreply.github.com>
2022-11-14 17:56:21 +0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,16 +1,3 @@
-// function tempConverter(a) {
-//   let n = parseInt(a);
-//   let unit = document.getElementById("converter");
-//   let selected = unit.options[unit.selectedIndex].value;
-//   if (isNaN(n)) {
-//     result.innerHTML = "Not a number";
-//   } else if (selected == "f2c") {
-//     result.innerHTML = (n - 32) * (5 / 9) + "°C";
-//   } else {
-//     result.innerHTML = n * 1.8 + 32 + "°F";
-//   }
-// }
-
 function convertCurrency(base, to) {
   const convertFunction = () =>
     fetch(`https://api.exchangerate.host/convert?from=${base}&to=${to}`).then(
