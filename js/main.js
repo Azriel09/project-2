ae2567e3c1d46b4c63167e7ee4270b78bd69c9e4
Azuriyale <Azuriyale@users.noreply.github.com>
2022-11-23 21:12:56 +0800

    added 2nd table and custom amount input for converting

--- js/main.js
+++ js/main.js
@@ -1,40 +1,7 @@
-var popularCurrencies = [
-  "PHP",
-  "USD",
-  "AUD",
-  "EUR",
-  "JPY",
-  "GBP",
-  "CAD",
-  "CNY",
-];
+let converter = new Currency();
 
-function convertCurrency(base, to) {
-  const convertFunction = () =>
-    fetch(`https://api.exchangerate.host/convert?from=${base}&to=${to}`).then(
-      (response) => response.json()
-    );
-
-  const data = convertFunction();
-
-  data.then((currencyValues) => {
-    let value = currencyValues["result"];
-    let currency = currencyValues["query"]["to"];
-    $("div.value-here").html(`${value} ${currency}`);
-  });
-}
-
-function onLoad2() {
-  var fromSelected = document.getElementById("base-currency");
-  var toSelected = document.getElementById("to-currency");
-  var baseCurrency = fromSelected.options[fromSelected.selectedIndex].value;
-  var toCurrency = toSelected.options[toSelected.selectedIndex].value;
-
-  convertCurrency(baseCurrency, toCurrency);
-  createGraph(baseCurrency);
-}
-
-function onLoad1() {
+function onLoad() {
+  // GETTING FROM API ALL COUNTRIES' SYMBOLS (Ex: USD, PHP, etc)
   var requestURL = "https://api.exchangerate.host/symbols";
   var request = new XMLHttpRequest();
   request.open("GET", requestURL);
@@ -43,7 +10,6 @@ function onLoad1() {
 
   request.onload = function () {
     var response = request.response;
-    console.log(response["symbols"]);
 
     // Making multiple currency options
     for (var [key, value] of Object.entries(response["symbols"])) {
@@ -61,18 +27,19 @@ function onLoad1() {
       option2.innerHTML = `${key} - ${description}`;
       option2.value = key;
 
+      // Making the PHP selected by default in the first option
       if (key == "PHP") {
         option1.selected = true;
         var baseContainer = document.getElementById("base-currency");
         baseContainer.appendChild(option1);
         var toContainer = document.getElementById("to-currency");
         toContainer.append(option2);
-        console.log(baseContainer.options[baseContainer.selectedIndex].value);
         continue;
       }
 
       baseContainer.appendChild(option1);
 
+      // Making the USD selected by default in the second option
       if (key == "USD") {
         option2.selected = true;
         var toContainer = document.getElementById("to-currency");
@@ -81,59 +48,40 @@ function onLoad1() {
       }
       toContainer.appendChild(option2);
     }
-    onLoad2();
+
+    // Creating a table
+    var fromSelected = document.getElementById("base-currency");
+    var toSelected = document.getElementById("to-currency");
+    var base = fromSelected.options[fromSelected.selectedIndex].value;
+    var to = toSelected.options[toSelected.selectedIndex].value;
+    converter.createTable1(base);
+    converter.createTable2(base, to);
+
+    // Convert amount function
+    document.getElementById("convert").onclick = () => {
+      var fromSelected = document.getElementById("base-currency");
+      var toSelected = document.getElementById("to-currency");
+      var base = fromSelected.options[fromSelected.selectedIndex].value;
+      var to = toSelected.options[toSelected.selectedIndex].value;
+      var amount = document.getElementById("amount").value;
+      converter.convertAmount(base, to, amount);
+    };
+
+    // Change the table content everytime the option is changed
+    document.getElementById("base-currency").onchange = () => {
+      var fromSelected = document.getElementById("base-currency");
+      var base = fromSelected.options[fromSelected.selectedIndex].value;
+      var base = fromSelected.options[fromSelected.selectedIndex].value;
+      var to = toSelected.options[toSelected.selectedIndex].value;
+      converter.changedOption(base, to);
+    };
+
+    document.getElementById("to-currency").onchange = () => {
+      var fromSelected = document.getElementById("base-currency");
+      var toSelected = document.getElementById("to-currency");
+      var base = fromSelected.options[fromSelected.selectedIndex].value;
+      var to = toSelected.options[toSelected.selectedIndex].value;
+      converter.changedOption(base, to);
+    };
   };
 }
-
-document
-  .getElementById("base-currency")
-  .addEventListener("change", changeOption);
-document.getElementById("to-currency").addEventListener("change", changeOption);
-
-function changeOption() {
-  var fromSelected = document.getElementById("base-currency");
-  var toSelected = document.getElementById("to-currency");
-  var baseCurrency = fromSelected.options[fromSelected.selectedIndex].value;
-  var toCurrency = toSelected.options[toSelected.selectedIndex].value;
-  convertCurrency(baseCurrency, toCurrency);
-  createGraph(baseCurrency);
-}
-
-function createGraph(base) {
-  latestRates(base);
-}
-
-function latestRates(base) {
-  const latestFunction = () =>
-    fetch(`https://api.exchangerate.host/latest?base=${base}`).then(
-      (response) => response.json()
-    );
-
-  const data = latestFunction();
-
-  data.then((currencyRates) => {
-    var rates = currencyRates["rates"];
-    var placer = 0;
-    $("td.baserate").html(`${base} 1`);
-    for (var [key, value] of Object.entries(rates)) {
-      for (i = 0; i < popularCurrencies.length; i++) {
-        if (key == popularCurrencies[i]) {
-          console.log(`Match! ${key}`);
-          $(`#rate${placer}`).html(`${key} ${value}`);
-          placer++;
-        }
-      }
-    }
-  });
-}
-
-//     for (i = 0; i < popularCurrencies.length; i++) {
-//       var table = document.getElementById("table-graph");
-//       var row = table.insertRow(i);
-//       var cell1 = row.insertCell(0);
-//       var cell2 = row.insertCell(1);
-//       cell1.innerHTML = `${base} 1`;
-//       cell2.innerHTML = popularCurrencies[i];
-//     }
-//   });
-// }
